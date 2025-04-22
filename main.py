b3e14273162f633a7e727dd30e7453099c4f2301
Per Olsen <persteen22@gmail.com>
2025-04-22 14:12:05 +0200

    Updated main

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ config = dotenv_values(".env")
 
 app = FastAPI()
 
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
-    print("Project connected to the MongoDB database!")
+app.mongodb_client = MongoClient(config["ATLAS_URI"])
+app.database = app.mongodb_client[config["DB_NAME"]]
+print("Project connected to the MongoDB database!")
 
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
+#@app.on_event("shutdown")
+#def shutdown_db_client():
+#    app.mongodb_client.close()
 
 app.include_router(api_router)
 
