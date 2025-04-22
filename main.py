b1a3633398153078fb69b56f77747e371ae9fd64
Per Olsen <persteen22@gmail.com>
2025-04-22 14:32:42 +0200

    Updated main to work at Vercel

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 from fastapi import FastAPI
 import uvicorn
-#from dotenv import dotenv_values
+
 import os
 import os.path
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from routes.api import router as api_router
-#config = dotenv_values(".env")
 
 app = FastAPI()
-
 
 load_dotenv()
 
 ATLAS_URI = os.getenv('ATLAS_URI')
-
 DB_NAME = os.getenv('DB_NAME')
-
 
 app.mongodb_client = MongoClient(ATLAS_URI)
 app.database = app.mongodb_client.get_database(DB_NAME)
 
 print("Project connected to the MongoDB database!")
-
-#@app.on_event("shutdown")
-#def shutdown_db_client():
-#    app.mongodb_client.close()
 
 app.include_router(api_router)
 
